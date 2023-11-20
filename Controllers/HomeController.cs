65acf3bdd79ec6ea41eeafed01799bb041be42a6
Remigio1 <rembiotec@gmail.com>
2023-11-20 10:53:23 -0400

    versão 07

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public IActionResult Prod1notebook()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
